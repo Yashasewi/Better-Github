1b755ca8d05e8729af3e64cfea6f6996a03bdce2
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-06-29 23:32:16 +0530

    fetching  the data

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -2,24 +2,11 @@ import Cards from "@/components/Cards";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Skeleton } from "@/components/ui/skeleton";
 
-import { cn } from "@/lib/utils";
+import Container from "@/components/ui/container";
+import Link from "next/link";
+import { UserData, params, repo } from "@/lib/types";
 
-function Container({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn(
-        " grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ",
-        className
-      )}
-      {...props}
-    />
-  );
-}
-
-async function getUserData(username: string) {
+async function getUserData(username: string): Promise<UserData> {
   const res = await fetch(`https://api.github.com/users/${username}`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -28,24 +15,36 @@ async function getUserData(username: string) {
   return data;
 }
 
-export default async function UserNamePage({
-  params,
-}: {
-  params: { username: string };
-}) {
+async function getRepoData(username: string): Promise<repo[]> {
+  const res = await fetch(`https://api.github.com/users/${username}/repos`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  return data;
+}
+
+export default async function UserNamePage({ params }: params) {
   const data = await getUserData(params.username);
 
-  console.log(data);
+  // console.log(data);
+
+  const repoData = await getRepoData(params.username);
+  // console.log(repoData);
+  console.log(repoData[1]);
+  console.log(repoData[1].name);
 
   return (
     <div>
       <div className="flex flex-col items-center justify-center mt-20 mb-12 gap-y-2">
-        <Avatar className="w-48 h-48">
-          <AvatarImage src={`${data.avatar_url}`} />
-          <AvatarFallback>
-            <Skeleton className="w-48 h-48 rounded-full" />
-          </AvatarFallback>
-        </Avatar>
+        <Link href={`${data.html_url}`}>
+          <Avatar className="w-48 h-48">
+            <AvatarImage src={`${data.avatar_url}`} />
+            <AvatarFallback>
+              <Skeleton className="w-48 h-48 rounded-full" />
+            </AvatarFallback>
+          </Avatar>
+        </Link>
         <div>
           <h1 className="text-2xl font-bold">{`@${params.username}`}</h1>
         </div>
@@ -63,14 +62,9 @@ export default async function UserNamePage({
       </div>
 
       <Container className="m-8 card_container">
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
+        {repoData.map((repo: repo) => (
+          <Cards key={repo.id} repo={repo} />
+        ))}
       </Container>
     </div>
   );
