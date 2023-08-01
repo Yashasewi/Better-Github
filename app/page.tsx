2c9b180abe7f93c0fe6a753a2466ef09653c2e3d
Yashasewi <Yashasewisingh2668@gmail.com>
2023-08-02 03:17:55 +0530

    fixing the button

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,57 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useRef, useState } from "react";
 
 export default function Home() {
-  let username = "";
-  const usernameRef = useRef<HTMLInputElement>(null);
-  const router = useRouter();
-  function getUsername() {
-    username = usernameRef.current?.value || "";
-    router.push(`/${username}`);
-    console.log(username);
-  }
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const [user, setUser] = useState("");
+    const [loading, setLoading] = useState(false);
+    function getUsername() {
+        setUser(usernameRef.current?.value || "");
+    }
 
-  return (
-    <div
-      className={cn(
-        " min-h-screen flex flex-col items-center justify-center gap-y-16"
-      )}
-    >
-      <h1 className="text-4xl font-bold text-center">
-        Enter your Github username
-      </h1>
+    return (
+        <div
+            className={cn(
+                " min-h-screen flex flex-col items-center justify-center gap-y-16"
+            )}
+        >
+            <h1 className="text-4xl font-bold text-center">
+                Enter your Github username
+            </h1>
 
-      <div className="flex items-center w-full max-w-sm space-x-2">
-        <Input type="text" placeholder="Username" ref={usernameRef} />
-        <Button type="submit" onClick={getUsername}>
-          Search
-        </Button>
-      </div>
-    </div>
-  );
+            <div className="flex items-center w-full max-w-sm space-x-2">
+                <Input
+                    type="text"
+                    placeholder="Username"
+                    ref={usernameRef}
+                    onChange={getUsername}
+                />
+                <Link
+                    href={{
+                        pathname: `/${user}`,
+                    }}
+                >
+                    <Button
+                        type="submit"
+                        onClick={() => {
+                            setLoading(true);
+                            setTimeout(() => {
+                                setLoading(false);
+                            }, 1000);
+                        }}
+                    >
+                        {loading ? (
+                            <Loader2 className="text-white  animate-spin" />
+                        ) : (
+                            "Search"
+                        )}
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
 }
